b23b87a301f822732f52388d678d665e7d89f251
asyuan0506 <asyuan0506@gmail.com>
2026-04-06 14:22:47 +0800

    Delete unnecessary domain

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['defai.asyuan.com', 'defai-omega.vercel.app'],
+  allowedDevOrigins: ['defai.asyuan.com'],
 };
 
 export default nextConfig;
